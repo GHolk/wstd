66106dea820de085aa50f18697a8154417bc3671
gholk <c34031328@mail.ncku.edu.tw>
2017-04-12 23:57:28 +0800

    wfk.m and opk2m.c finish

--- opk2m.c
+++ opk2m.c
@@ -6,8 +6,25 @@
 #define PHI 1
 #define KAPA 2
 
+#define M_OMEGA(omega) { \
+    {1, 0, 0}, \
+    {0, cos(omega), sin(omega)}, \
+    {0, -sin(omega), cos(omega)} \
+}
+
+#define M_PHI(phi) { \
+    {cos(phi), 0, -sin(phi)}, \
+    {0, 1, 0}, \
+    {sin(phi), 0, cos(phi)} \
+}
+
+#define M_KAPA(kapa) { \
+    {cos(degree[KAPA]), sin(degree[KAPA]), 0}, \
+    {-sin(degree[KAPA]), cos(degree[KAPA]), 0}, \
+    {0, 0, 1} \
+}
+
 typedef double Matrix3[3][3];
-typedef double Vector3[3];
 
 void multiply_matrix (Matrix3 m1, Matrix3 m2, Matrix3 answer) {
     for (int i=0; i<3; i++) {
@@ -41,21 +58,9 @@ int main(int argc, char* argv[]) {
     }
 
     Matrix3 m[] = {
-        { // M(omega)
-            {1, 0, 0},
-            {0, cos(degree[OMEGA]), sin(degree[OMEGA])},
-            {0, -sin(degree[OMEGA]), cos(degree[OMEGA])}
-        },
-        { // M(phi)
-            {cos(degree[PHI]), 0, -sin(degree[PHI])},
-            {0, 1, 0},
-            {sin(degree[PHI]), 0, cos(degree[PHI])}
-        },
-        { // M(kapa)
-            {cos(degree[KAPA]), sin(degree[KAPA]), 0},
-            {-sin(degree[KAPA]), cos(degree[KAPA]), 0},
-            {0, 0, 1}
-        }
+        M_OMEGA(degree[OMEGA]),
+        M_PHI(degree[PHI]),
+        M_KAPA(degree[KAPA])
     };
 
     puts("M(omega) = ");
@@ -73,6 +78,7 @@ int main(int argc, char* argv[]) {
     multiply_matrix(m[KAPA], m[PHI], answer[0]);
     multiply_matrix(answer[0], m[OMEGA], answer[1]);
     print_matrix(answer[1]);
+
     return 0;
 }
 
--- wfk.m
+++ wfk.m
@@ -1,44 +1,36 @@
-# Created by Octave 4.0.3, Wed Apr 12 21:55:24 2017 CST <c34031328@hmjs>
-# name: mw
-# type: function handle
-@<anonymous>
-@(w) [1, 0, 0; 0, cos(w), sin(w); 0, -sin(w), cos(w)]
+#!/usr/bin/env octave
+
+mw = @(w) ([
+    1 0 0
+    0 cos(w) sin(w)
+    0 -sin(w) cos(w)
+]);
 
 
-# name: mf
-# type: function handle
-@<anonymous>
-@(f) [cos(f), 0, -sin(f); 0, 1, 0; sin(f), 0, cos(f)]
+mf = @(f) ([
+    cos(f) 0 -sin(f)
+    0 1 0
+    sin(f) 0 cos(f)
+]);
 
+mk = @(k) ([
+    cos(k) sin(k) 0
+    -sin(k) cos(k) 0
+    0 0 1
+]);
 
-# name: mk
-# type: function handle
-@<anonymous>
-@(k) [cos(k), sin(k), 0; -sin(k), cos(k), 0; 0, 0, 1]
+wfk = @(w, f, k) mk (k) * mf (f) * mw (w);
 
+if size(argv()) == [3 1]
+    degree = zeros(1,3);
+    for i = 1:3
+        degree(i) = str2num(argv(){i});
+    end
+else
+    do
+        degree = input("please input [omega phi kapa]: \n");
+    until size(degree) == [1 3]
+end
 
-# name: wfk
-# type: function handle
-@<anonymous>
-@(w, f, k) mk (k) * mf (f) * mw (w)
-# length: 3
-# name: mf
-# type: function handle
-@<anonymous>
-@(f) [cos(f), 0, -sin(f); 0, 1, 0; sin(f), 0, cos(f)]
-
-
-# name: mk
-# type: function handle
-@<anonymous>
-@(k) [cos(k), sin(k), 0; -sin(k), cos(k), 0; 0, 0, 1]
-
-
-# name: mw
-# type: function handle
-@<anonymous>
-@(w) [1, 0, 0; 0, cos(w), sin(w); 0, -sin(w), cos(w)]
-
-
-
+disp(mk(degree(3)) * mf(degree(2)) * mw(degree(1)));
 
