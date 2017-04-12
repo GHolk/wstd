dfe4df828c59a7f0651dd193798cb1b29f8272e3
gholk <c34031328@mail.ncku.edu.tw>
2017-04-12 17:36:39 +0800

    use stupid method

--- opk2m.c
+++ opk2m.c
@@ -3,33 +3,16 @@
 #include <stdlib.h>
 #include <math.h>
 
-int walk(double m[3][3], int (*operate)(double *v)) {
-    for (int i=0; i<3; i++) {
-        for (int j=0; j<3; j++) {
-            operate(&m[i][j]);
-        }
-    }
-    return 0;
-}
-
-int print_double_pointer(double *v) {
-    printf("%lf\t", *v);
-    return 0;
-}
-
-int put_in_double_pointer(double *v) {
-    return scanf("%15lf", v) > 0;
-}
-
 int main(int argc, char* argv[]) {
-    double m[3][3];
-    int (*print)(double *v), (*put)(double *v);
-    print = print_double_pointer;
-    put = put_in_double_pointer;
+    double m[3][3], opk[3];
+    if (argc-1 == 3) {
+        for (int i=0; i<3; i++) sscanf(argv[i+1], "%lf", &opk[i]);
+    }
+    else {
+        for (int i=0; i<3; i++) scanf("%lf", &opk[i]);
+    }
 
-    walk(m, put);
-    walk(m, print);
-
+    printf("%lf\t%lf\t%lf\n", opk[0], opk[1], opk[2]);
     return 0;
 }
 
