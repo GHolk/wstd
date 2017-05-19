44b1099ed35c0d20daba34fad8a9dd29e2b810c4
gholk <c34031328@mail.ncku.edu.tw>
2017-05-19 23:59:48 +0800

    error file with lens distortion

--- photo-remote/rotate-program/icfs/wrap_gnuplot.sh
+++ photo-remote/rotate-program/icfs/wrap_gnuplot.sh
@@ -2,9 +2,9 @@
 file="$1"
 
 gnuplot <<GPLOT
-set output "$file.svg"
-set term svg
-plot "$file" using 2:3:5:6 with vectors title "$file"
+    set output "$file.svg"
+    set term svg
+    plot "$file" using 2:3:5:6 with vectors title "$file"
 GPLOT
 
 
